1463b856fb27f8c1a63599c26fb32b03bc2b66d1
Andrea Lattuada <andreal@student.ethz.ch>
2015-10-30 00:48:59 +0100

    somewhat working

---
+++ examples/chunked_flat_map_exp.rs
@@ -0,0 +1,44 @@
+extern crate mmap;
+extern crate time;
+extern crate timely;
+extern crate dataflow_join;
+
+use std::rc::Rc;
+
+use dataflow_join::*;
+use dataflow_join::graph::{GraphTrait, GraphMMap, GraphExtenderExt};
+use dataflow_join::chunked_map::ChunkedMap;
+
+use timely::dataflow::*;
+use timely::dataflow::operators::*;
+
+// use timely::communication::Communicator;
+
+fn main () {
+
+    timely::execute_from_args(std::env::args().skip(4), move |root| {
+
+        let mut input = root.scoped(|builder| {
+
+            let (input, stream) = builder.new_input::<u32>();
+
+            let flatmapped = stream.chunked_flat_map(|i| vec![(i, i + 1), (i, i + 2), (i, i + 3)].into_iter());
+
+            flatmapped.inspect(|&(a, b)| println!("{},{}", a, b));
+
+            input
+        });
+
+
+        for i in 0..100 {
+            input.send(i);
+            if (i % 10 == 0) {
+                input.advance_to((i / 10) as u64 + 1);
+            }
+            root.step();
+        }
+
+        input.close();
+        while root.step() { }
+    })
+}
--- src/chunked_map.rs
+++ src/chunked_map.rs
@@ -6,6 +6,7 @@ use std::slice::Iter;
 use std::ops::RangeFull;
 use std::fmt::Debug;
 use std::collections::LinkedList;
+use std::iter::FromIterator;
 
 use timely::dataflow::channels::Content;
 use timely::dataflow::{Stream, Scope};
@@ -28,25 +29,37 @@ pub trait ChunkedMap<S: Scope, D: Data> {
     ///            .inspect(|x| println!("seen: {:?}", x));
     /// });
     /// ```
-    fn chunked_flat_map<I: Iterator, L: Fn(D)->I+'static>(&self, logic: L) -> Stream<S, I::Item> where I::Item: Data, D: Debug;
+    fn chunked_flat_map<I: Iterator, L: Fn(D)->I+'static>(&self, logic: L) -> Stream<S, I::Item> where I::Item: Data, D: Debug, I::Item: Debug;
 }
 
 impl<S: Scope, D: Data> ChunkedMap<S, D> for Stream<S, D> {
     fn chunked_flat_map<I: Iterator, L: Fn(D)->I+'static>(&self, logic: L) -> Stream<S, I::Item>
-    where I::Item: Data, D: Debug {
-        let mut stash: Option<(S::Timestamp, Vec<D>)> = None;
+    where I::Item: Data, D: Debug, I::Item: Debug {
+        let mut stashed_time: Option<S::Timestamp> = None;
+        let mut in_data = LinkedList::<D>::new();
+        let mut out_data = LinkedList::<I::Item>::new();
         self.unary_stream(Pipeline, "ChunkedFlatMap", move |input, output| {
-            match stash.clone() {
-                Some((time, ref mut data)) => {
-                    let mut drain = data.drain_temp();
-                    if let Some(x) = drain.next() {
-                        output.session(&time).give_iterator(logic(x));
+            for _ in 0..100 {
+                match stashed_time {
+                    Some(t) => {
+                        let out_datum: Option<I::Item> = if let Some(datum) = out_data.pop_front() {
+                            Some(datum)
+                        } else if let Some(x) = in_data.pop_front() {
+                            out_data = logic(x).collect::<LinkedList<I::Item>>();
+                            out_data.pop_front()
+                        } else {
+                            stashed_time = None;
+                            break;
+                            None
+                        };
+                        if let Some(datum) = out_datum {
+                            output.session(&t).give(datum);
+                        }
+                    },
+                    None => if let Some((time, data)) = input.next() {
+                        in_data = data.drain_temp().collect::<LinkedList<D>>();
+                        stashed_time = Some((*time).clone());
                     }
-                },
-                None => if let Some((time, data)) = input.next() {
-                    let mut vec = Vec::new();
-                    for datum in data.drain_temp() { vec.push(datum); }
-                    stash = Some(((*time).clone(), vec));
                 }
             }
         })
